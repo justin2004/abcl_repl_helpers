9766952d40260b6b0b5e7b21e6ad603bb12773c2
Justin <justin2004@hotmail.com>
2020-03-24 08:16:29 -0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## ABCL REPL helpers
+
+- These are just some functions that I find to be helpful when I am exploring a java library at the REPL. 
---
+++ helpers.lisp
@@ -0,0 +1,32 @@
+(defun m-sum (m)
+  "method summary"
+  (let ((name (jmethod-name m))
+        (ret (jclass-name (jmethod-return-type m)))
+        (params (mapcar #'jclass-name
+                        (map 'list #'identity
+                             (jmethod-params m)))))
+    (list name ret params)))
+        
+
+(defun c-sum (cname)
+  "class summary -- given a class name (or java-object) print its methods with their sigs"
+  (let* ((c (if (typep cname 'java:java-object)
+                ;(jclass-of cname)   ; this is wrong -- it turned a QueryFactory to java.lang.Class
+                cname
+                (jclass cname)))
+         (methods (map 'list 
+                       #'identity
+                       (jclass-methods c)))
+         (summarized-methods (mapcar #'m-sum
+                                     methods)))
+    (mapcar #'(lambda (method-summary)
+                (format t "~S ~S~%  ~S~%~%" 
+                        (car method-summary) ;name
+                        (cadr method-summary) ;return
+                        (caddr method-summary) ; args
+                        ))
+            summarized-methods)
+    nil))
+
+; e.g (c-sum "org.apache.jena.rdf.model.ModelFactory")
+
